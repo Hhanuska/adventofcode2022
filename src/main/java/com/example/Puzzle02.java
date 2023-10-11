8c5bbd82ad0e1c9c8a1785475f3c0b5b1671185a
Hhanuska <41262567+Hhanuska@users.noreply.github.com>
2023-10-11 18:05:59 +0200

    feat: puzzle02/2

--- src/main/java/com/example/Puzzle02.java
+++ src/main/java/com/example/Puzzle02.java
@@ -5,7 +5,9 @@ import java.io.FileNotFoundException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 public class Puzzle02 {
@@ -22,9 +24,17 @@ public class Puzzle02 {
     pointMap.put('C', 3);
   }
 
+  private static HashMap<Character, Integer> outcomeMap;
+  static {
+    outcomeMap = new HashMap<>();
+    outcomeMap.put('X', 0);
+    outcomeMap.put('Y', 3);
+    outcomeMap.put('Z', 6);
+  }
+
   public static void main(String[] args) throws URISyntaxException, FileNotFoundException {
     System.out.println("Solution 1: " + solve01());
-    // System.out.println("Solution 1: " + solve02());
+    System.out.println("Solution 1: " + solve02());
   }
 
   public static int getPointsForRound(char you, char opponent) {
@@ -51,6 +61,51 @@ public class Puzzle02 {
     while (scanner.hasNextLine()) {
       String line = scanner.nextLine();
       totalPoints += getPointsForRound(line.charAt(2), line.charAt(0));
+    }
+    scanner.close();
+
+    return totalPoints;
+  }
+
+  private static List<Character> orderedChars = Arrays.asList('A', 'B', 'C');
+
+  public static char toPick(char opponent, char todo) throws IllegalArgumentException {
+    int opponentIndex = orderedChars.indexOf(opponent);
+
+    if (todo == 'X') {
+      if (opponentIndex > 0) {
+        return orderedChars.get(opponentIndex - 1);
+      } else {
+        return orderedChars.get(opponentIndex + 2);
+      }
+    }
+
+    if (todo == 'Y') {
+      return opponent;
+    }
+
+    if (todo == 'Z') {
+      if (opponentIndex < 2) {
+        return orderedChars.get(opponentIndex + 1);
+      } else {
+        return orderedChars.get(opponentIndex - 2);
+      }
+    }
+
+    throw new IllegalArgumentException("Todo must equal to X, Y or Z");
+  }
+
+  public static int solve02() throws URISyntaxException, FileNotFoundException {
+    File file = Paths.get(resource.toURI()).toFile();
+    Scanner scanner = new Scanner(file);
+
+    int totalPoints = 0;
+
+    while (scanner.hasNextLine()) {
+      String line = scanner.nextLine();
+      char toPick = Puzzle02.toPick(line.charAt(0), line.charAt(2));
+      int toAdd = pointMap.get(toPick) + outcomeMap.get(line.charAt(2));
+      totalPoints += toAdd;
     }
     scanner.close();
 
