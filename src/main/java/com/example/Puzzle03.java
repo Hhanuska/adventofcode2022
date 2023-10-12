05ca28aa1572d20ddb9861a6e63bdb6ebccd8292
Hhanuska <41262567+Hhanuska@users.noreply.github.com>
2023-10-12 10:14:50 +0200

    feat: puzzle03/2

--- src/main/java/com/example/Puzzle03.java
+++ src/main/java/com/example/Puzzle03.java
@@ -5,6 +5,8 @@ import java.io.FileNotFoundException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Puzzle03 {
@@ -12,7 +14,7 @@ public class Puzzle03 {
 
   public static void main(String[] args) throws URISyntaxException, FileNotFoundException {
     System.out.println("Solution 1: " + solve01());
-    // System.out.println("Solution 1: " + solve02());
+    System.out.println("Solution 1: " + solve02());
   }
 
   public static int getPriority(char character) {
@@ -50,6 +52,40 @@ public class Puzzle03 {
       }
     }
     scanner.close();
+
+    return priorityTotal;
+  }
+
+  public static List<String> readFile() throws URISyntaxException, FileNotFoundException {
+    File file = Paths.get(resource.toURI()).toFile();
+    Scanner scanner = new Scanner(file);
+
+    List<String> lines = new ArrayList<String>();
+
+    while (scanner.hasNextLine()) {
+      lines.add(scanner.nextLine());
+    }
+    scanner.close();
+
+    return lines;
+  }
+
+  public static int solve02() throws URISyntaxException, FileNotFoundException {
+    List<String> lines = readFile();
+
+    int priorityTotal = 0;
+
+    for (int i = 2; i < lines.size(); i = i + 3) {
+      String line = lines.get(i);
+      for (int j = 0; j < line.length(); j++) {
+        char currentChar = line.charAt(j);
+
+        if (lines.get(i - 1).indexOf(currentChar) != -1 && lines.get(i - 2).indexOf(currentChar) != -1) {
+          priorityTotal += getPriority(currentChar);
+          break;
+        }
+      }
+    }
 
     return priorityTotal;
   }
