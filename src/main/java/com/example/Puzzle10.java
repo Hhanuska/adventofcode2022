6384c98dcfc2fd794bff68d2a2944ddcb37c5272
Hhanuska <41262567+Hhanuska@users.noreply.github.com>
2023-10-14 19:26:58 +0200

    feat: puzzle10/2

--- src/main/java/com/example/Puzzle10.java
+++ src/main/java/com/example/Puzzle10.java
@@ -12,7 +12,8 @@ public class Puzzle10 {
 
   public static void main(String[] args) throws URISyntaxException, IOException {
     System.out.println("Solution 1: " + solve01());
-    // System.out.println("Solution 2: " + solve02());
+    System.out.println("Solution 2:");
+    solve02();
   }
 
   public static int solve01() throws URISyntaxException, IOException {
@@ -43,5 +44,58 @@ public class Puzzle10 {
     }
 
     return sum;
+  }
+
+  public static void solve02() throws URISyntaxException, IOException {
+    List<String> lines = Files.readAllLines(Paths.get(resource.toURI()));
+
+    CRT crt = new CRT();
+
+    int register = 1;
+
+    for (String line : lines) {
+      String[] parts = line.split(" ");
+      crt.incrCycle(register);
+
+      if (parts[0].equals("noop")) {
+        continue;
+      }
+
+      // parts[1].equals("addx");
+      crt.incrCycle(register);
+
+      register += Integer.parseInt(parts[1]);
+    }
+
+    crt.print();
+  }
+
+  private static class CRT {
+    private char[][] crt = new char[6][40];
+
+    private int cycle = 0;
+
+    public void incrCycle(int register) {
+      // use old value in this.draw(), then increment
+      this.draw(this.cycle++, register);
+    }
+
+    private void draw(int cycle, int register) {
+      int row = cycle / 40;
+      int col = cycle % 40;
+
+      if (Math.abs(col - register) <= 1) {
+        crt[row][col] = '#';
+      } else {
+        // use whitespace instead of '.' for better readability
+        crt[row][col] = ' ';
+      }
+    }
+
+    public void print() {
+      for (char[] row : crt) {
+        System.out.println(row);
+      }
+    }
   }
 }
