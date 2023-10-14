3071715dac78985711d7f6e492c0d17bf512ca1a
Hhanuska <41262567+Hhanuska@users.noreply.github.com>
2023-10-14 16:14:44 +0200

    feat: puzzle09/1

---
+++ src/main/java/com/example/Puzzle09.java
@@ -0,0 +1,93 @@
+package com.example;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Puzzle09 {
+  private static URL resource = Puzzle09.class.getResource("/puzzleinput09.txt");
+
+  public static void main(String[] args) throws URISyntaxException, FileNotFoundException {
+    System.out.println("Solution 1: " + solve01());
+    // System.out.println("Solution 2: " + solve02());
+  }
+
+  public static int solve01() throws URISyntaxException, FileNotFoundException {
+    File file = Paths.get(resource.toURI()).toFile();
+    Scanner scanner = new Scanner(file);
+
+    RopeSim sim = new RopeSim();
+    HashSet<String> uniqueTailPositions = new HashSet<>();
+    uniqueTailPositions.add(sim.getTailPosition());
+
+    while (scanner.hasNextLine()) {
+      String line = scanner.nextLine();
+      String[] parts = line.split(" ");
+
+      for (int i = 0; i < Integer.parseInt(parts[1]); i++) {
+        sim.move(parts[0].charAt(0));
+        uniqueTailPositions.add(sim.getTailPosition());
+      }
+    }
+    scanner.close();
+
+    return uniqueTailPositions.size();
+  }
+
+  private static class RopeSim {
+    private class Position {
+      int x = 0;
+      int y = 0;
+    }
+
+    Position head = new Position();
+    Position tail = new Position();
+
+    public void move(char dir) {
+      moveHead(dir);
+      moveTail();
+    }
+
+    public String getTailPosition() {
+      return tail.x + "-" + tail.y;
+    }
+
+    private void moveHead(char dir) {
+      switch (dir) {
+        case 'U':
+          head.y++;
+          break;
+        case 'D':
+          head.y--;
+          break;
+        case 'L':
+          head.x--;
+          break;
+        case 'R':
+          head.x++;
+          break;
+        default:
+          throw new IllegalArgumentException();
+      }
+    }
+
+    private void moveTail() {
+      int dist = Math.abs(this.head.x - this.tail.x) + Math.abs(this.head.y - this.tail.y);
+
+      boolean moveRequired = dist > 2 || (dist == 2 && (head.x == tail.x || head.y == tail.y));
+
+      if (!moveRequired) {
+        return;
+      }
+
+      if (head.x != tail.x)
+        tail.x = head.x > tail.x ? tail.x + 1 : tail.x - 1;
+      if (head.y != tail.y)
+        tail.y = head.y > tail.y ? tail.y + 1 : tail.y - 1;
+    }
+  }
+}
