3a0c3066235cbfb3e1ad458bc8e1fec9fcc8da11
Hhanuska <41262567+Hhanuska@users.noreply.github.com>
2023-10-14 15:31:14 +0200

    fix: remove unused exception

--- src/main/java/com/example/Puzzle07.java
+++ src/main/java/com/example/Puzzle07.java
@@ -2,7 +2,6 @@ package com.example;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Paths;
